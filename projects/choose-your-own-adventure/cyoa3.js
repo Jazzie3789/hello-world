1e0562f64b710fdd282292f9be1dacfbabb82507
Jasmine.Thomas <Jazzie3789@yahoo.com>
2018-03-20 02:53:31 -0400

    Tried to help Sandbox and CYOA

--- projects/choose-your-own-adventure/cyoa3.js
+++ projects/choose-your-own-adventure/cyoa3.js
@@ -7,7 +7,7 @@ function updateStory() {
 };
 
 
-var storyOutput = "Hey! This is an interactive story. Do you want to take part?";
+var storyOutput = "Hey! This is an interactive story. Do you want to take part? YES or NO?";
 updateStory();
 
 document.getElementById('javascriptButton').onclick = function interpretAnswer() {
@@ -19,14 +19,14 @@ document.getElementById('answerField').value;
 
 function checkAnswer() {
   if (answer === "YES") {
-    storyOutput = "Excellent!";
+    storyOutput = "Excellent! All you have to do is read the story and type in one of the choices given at the end of each chapter. Make sure you type it in exactly as it appears. Ready to start?";
   }
-  else if (answer === "CAN I THINK ABOUT IT?")
-{
-    storyOutput = "Sure! Type something else in whenever you're ready.";
-  }
-  else if (answer === 'NO') {
-    storyOutput = "Aw! Okay. See you later." ;
+    else if (answer === "YES")
+    {
+      storyOutput = "You walk into an old decrepit house. The plaster is falling off of the wall and there is an unusual stench that permeats the air. There is a hallway in front of you and stairs to your right. Type 'hallway' to go down the hallway, type 'stairs' to go up the stairs";
+    }
+  else if (answer === "NO"){
+    storyOutput = "Aww. Well, that's too bad. If you're too scared to read the story....fine. Pfft!";
   }
   else {
     storyOutput = "Hey! This is an interactive story. Do you want to take part? YES or NO";
--- projects/sandbox/sandbox.js
+++ projects/sandbox/sandbox.js
@@ -1,1 +1,28 @@
 
+
+
+$("h1").css("color", "blue");
+
+$("body").css("background-color", "lightcyan");
+
+$("#everything").css("font-size", "120%");
+
+$(".holder").css("border", "2px solid black");
+
+$("li").css("font-weight", "bold");
+
+$("li").css("color", "purple");
+
+$("h1+p").css("color", "darkgreen");
+
+$("P").prepend("Behold, ").append("!");
+
+$('ol li').fadeIn() // fade in a set of matched elements over setTimeout(function () {
+  [object Object]
+}, 10);
+
+$('p .comments')fadeOut() //fade out as a set of matched elements over setTimeout(function () {
+  [object Object]
+}, 10);
+
+$('#change-me').animate("font-size", "200%") // animate a CSS property of your choosing!
